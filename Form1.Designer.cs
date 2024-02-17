c46ec2bd62783021861a98b9c62e9bbd02566705
NazrenG <nezrin.qu@gmail.com>
2024-02-17 16:31:21 +0400

    Thread task

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ThreadsTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.copy_btn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.resume_btn = new System.Windows.Forms.Button();
+            this.to_button = new System.Windows.Forms.Button();
+            this.from_btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // copy_btn
+            // 
+            this.copy_btn.BackColor = System.Drawing.Color.CadetBlue;
+            this.copy_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copy_btn.Location = new System.Drawing.Point(375, 140);
+            this.copy_btn.Name = "copy_btn";
+            this.copy_btn.Size = new System.Drawing.Size(70, 31);
+            this.copy_btn.TabIndex = 1;
+            this.copy_btn.Text = "Copy";
+            this.copy_btn.UseVisualStyleBackColor = false;
+            this.copy_btn.Click += new System.EventHandler(this.copy_btn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Teal;
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.progressBar1.Location = new System.Drawing.Point(39, 148);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(330, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CadetBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(211, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Abort";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CadetBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(39, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 39);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Suspend";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // resume_btn
+            // 
+            this.resume_btn.BackColor = System.Drawing.Color.CadetBlue;
+            this.resume_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resume_btn.Location = new System.Drawing.Point(123, 186);
+            this.resume_btn.Name = "resume_btn";
+            this.resume_btn.Size = new System.Drawing.Size(69, 39);
+            this.resume_btn.TabIndex = 1;
+            this.resume_btn.Text = "Resume";
+            this.resume_btn.UseVisualStyleBackColor = false;
+            this.resume_btn.Click += new System.EventHandler(this.resume_btn_Click);
+            // 
+            // to_button
+            // 
+            this.to_button.BackColor = System.Drawing.Color.LightBlue;
+            this.to_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.to_button.Location = new System.Drawing.Point(375, 106);
+            this.to_button.Name = "to_button";
+            this.to_button.Size = new System.Drawing.Size(70, 23);
+            this.to_button.TabIndex = 1;
+            this.to_button.Text = "File to....";
+            this.to_button.UseVisualStyleBackColor = false;
+            this.to_button.Click += new System.EventHandler(this.to_button_Click);
+            // 
+            // from_btn
+            // 
+            this.from_btn.BackColor = System.Drawing.Color.LightBlue;
+            this.from_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.from_btn.Location = new System.Drawing.Point(375, 69);
+            this.from_btn.Name = "from_btn";
+            this.from_btn.Size = new System.Drawing.Size(70, 23);
+            this.from_btn.TabIndex = 1;
+            this.from_btn.Text = "File to ...";
+            this.from_btn.UseVisualStyleBackColor = false;
+            this.from_btn.Click += new System.EventHandler(this.from_btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "From";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 247);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.resume_btn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.from_btn);
+            this.Controls.Add(this.to_button);
+            this.Controls.Add(this.copy_btn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button copy_btn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button resume_btn;
+        private System.Windows.Forms.Button to_button;
+        private System.Windows.Forms.Button from_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadsTask
+{
+    public partial class Form1 : Form
+    {
+        public string srcPath, destPath;
+        public Thread thread;
+        public bool checkCopy = false;
+        public bool checkSuspend = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void from_btn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "AllFiles|*.*|TextFile|*.txt";
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName.ToString();
+                srcPath = textBox1.Text.ToString();
+            }
+        }
+
+        private void to_button_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "AllFiles|*.*|TextFile|*.txt";
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName.ToString();
+                destPath = textBox2.Text.ToString();
+            }
+        }
+
+        private void resume_btn_Click(object sender, EventArgs e)
+        {
+            if (!checkCopy)
+            {
+                MessageBox.Show("If you want a resume, you need to copy to a file first", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (checkSuspend)
+            {
+                thread.Resume();
+            }
+            else
+            {
+                MessageBox.Show("There are no suspended threads to resume!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!checkCopy)
+            {
+                MessageBox.Show("If you want a pause, you need to copy to a file first", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            thread.Suspend();
+            checkSuspend = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!checkCopy)
+            {
+                MessageBox.Show("If you want abort thread, you need to copy to a file first", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            thread.Abort();
+        }
+
+        private void copy_btn_Click(object sender, EventArgs e)
+        {
+
+            if (srcPath==null)
+            {
+                MessageBox.Show("The file to transfer does not exist!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (destPath==null)
+            {
+                MessageBox.Show("No file to file transfer!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            thread = new Thread(() =>
+           {
+               if (!File.Exists(srcPath))
+               {
+                   MessageBox.Show("File not exist!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                   return;
+               } 
+               progressBar1.Minimum = 1;
+               progressBar1.Value = 1;
+
+               using (FileStream fsRead = new FileStream(srcPath, FileMode.Open, FileAccess.Read))
+               {
+                   checkCopy = true;
+                   progressBar1.Maximum = Convert.ToInt32(fsRead.Length);
+
+                   using (FileStream fsWrite = new FileStream(destPath, FileMode.Create, FileAccess.Write))
+                   {
+                       var len = 10;
+                       var fileSize = fsRead.Length;
+                       byte[] buffer = new byte[len];
+
+                       do
+                       {
+                           len = fsRead.Read(buffer, 0, buffer.Length);
+                           progressBar1.Step = len;
+                           fsWrite.Write(buffer, 0, len);
+
+                           progressBar1.PerformStep();
+                           fileSize -= len;
+
+                           Thread.Sleep(250);
+
+                       } while (len != 0);
+
+                   }
+               }
+
+           });
+
+            thread.Start();
+
+            int final = progressBar1.Value;
+            if (final == progressBar1.Maximum)
+            {
+                progressBar1.Value = 1;
+                textBox1.Text = null;
+                textBox2.Text = "";
+            }
+        }
+    }
+}
